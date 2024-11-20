fa5b7144d3a21f6c61b4ed18e57f1e24efa1da53
nsaisnrtiunJS <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-11-20 09:34:28 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 4+3 )  // We are using Nodejs not Browser
+
+console.log( 3
+    +
+    4 ); // Code readability should be high
+
+    console.log( "Sam" );
+
+
+    let name = "Sam";
+    let  age = 18;
+    let isLoggedIn = false;
+    let state;
+
+
+    // Number =>2 to power 53
+    // bigInt
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined
+    // symbol => unique
+
+    // object
+
+    console.log(typeof undefined); // undefined
+    console.log( typeof null); // object
