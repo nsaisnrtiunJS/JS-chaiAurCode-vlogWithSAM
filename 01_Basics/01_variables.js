18255a6ce89b7ee826c271d0d077bb9a095e6de0
nsaisnrtiunJS <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-11-20 09:37:31 +0000

    done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 45454567;
+let accountEmail = "sam@gmail.com";
+var accountPassword = "123321";
+accountCity = "jaipur";
+
+//  accountId = 243243;
+/*
+Prefer not to use Var
+because of issue in Block scope and Functional scope
+*/
+
+accountEmail = "s@gmail.com";
+accountPassword = "434343";
+accountCity = "Malda";
+
+console.table([accountId, accountEmail,accountPassword,accountCity]);
