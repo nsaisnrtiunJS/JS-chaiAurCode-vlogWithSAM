43080b5ca22aebb281d6cbe51ced5959967e3e0f
nsaisnrtiunJS <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-11-27 09:31:14 +0000

    conversionOperation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,3 +1,46 @@
-let score = 32;
+let score = "Sam";
+// let score = 32;
+// let score = 32
+// let score = null
+// let score = undefined;
+// let score = Boolean;
+// let score = true;
+// let score = false;
+
 
 console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score);
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+        //   NOTES
+// "32" => 32; easily it is converted
+// "32ns" => NaN;  but basically it is a number
+// true => 1; and  False => 0;
+
+
+// let isLoggedIn = 1;
+// let isLoggedIn = 0;
+// let isLoggedIn = "";
+let isLoggedIn = "sam";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+        //    NOTE
+    // 1 => true; and 0 => false;    
+    // "" => false;   if it is empty then value will show false
+    //    "Sam" => true;   if I write something then value will show true
+
+
+
+
+    let anyNumber = 32;
+    let stringNumber = String(anyNumber);
+    console.log(stringNumber);
+    console.log(typeof stringNumber);
