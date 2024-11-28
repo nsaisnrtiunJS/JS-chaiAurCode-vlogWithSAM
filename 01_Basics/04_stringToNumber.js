754a2cf98916f37fe9e5a811c5a5f5532a310048
nsaisnrtiunJS <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-11-28 15:24:11 +0000

    stringToNumber

---
+++ 01_Basics/04_stringToNumber.js
@@ -0,0 +1,56 @@
+// let toNumber = "Sam";
+// let stringToNumber = Number(toNumber);
+
+// console.log(stringToNumber);
+
+
+
+
+            // OPERATIONS
+
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let num1 = "hello"
+let num2 = " Sam"
+let num3 = num1 + num2;
+console.log(num3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+// console.log(2 + "2" + 1);
+
+
+
+
+// console.log(+true);
+// console.log(+"")
+
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+//  gameCounter++;
+//  ++gameCounter;
+// console.log(gameCounter);
+
+
+let a = 5;
+// note:- it returns the value after increment/Decrement.
+// let b = ++a;
+let b = --a ;   
+
+// note:- it returns the value after increment/Decrement.
+// let b = a++;
+// let b = a--;
+
+console.log(`a:${a}, b:${b}`);
+// console.log(a)
+// console.log(b)
