73ed079413f64298344ed7e5a93574776c90530b
nsaisnrtiunJS <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-11-29 08:40:11 +0000

    comparison

---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,33 @@
+// console.log(2>1);
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2<=1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+// console.log(2 == 2);
+
+
+
+
+// AVOID THIS ALSO
+
+/* console.log("3" > 1)
+   console.log("3" < 1)
+   console.log("3" >= 1)
+*/
+
+
+
+
+
+
+//    DON'T DO THESE TYPE OF COMPARISON
+
+/* console.log(null > 0)
+   console.log(null < 0)
+ console.log(null >= 0)
+ console.log(null == 0)
+
+ console.log(undefined > 0)
+ console.log(undefined == 0)
+ console.log(undefined >= 0) */
