6bd345eec1f54b5c4c64bbe342247d58667dd682
nsaisnrtiunJS <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-11-29 11:00:31 +0000

    datatypes

---
+++ 01_Basics/06_datatypes_summary.js
@@ -0,0 +1,16 @@
+// There are 2 types of Data types. 1. Primitive and 2. Non-Primitive
+
+// 1.Primitive:
+
+/* Primitive data types divided by 7 data classes.and these 
+are:- Number, Boolean, string, null, undefined, symbol, bigInt.
+*/
+
+
+// 2.Reference(Non-Primitive):
+// 3types:- Array,Object and Function
+
+
+
+
+
