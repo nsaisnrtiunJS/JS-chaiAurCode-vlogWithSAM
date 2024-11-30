a05b1477e1601adae1ae19971e362938fcd71384
SamCodrr <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-11-30 16:04:43 +0000

    datatypes_summary

--- 01_Basics/06_datatypes_summary.js
+++ 01_Basics/06_datatypes_summary.js
@@ -6,6 +6,25 @@
 are:- Number, Boolean, string, null, undefined, symbol, bigInt.
 */
 
+// All dataType's example
+// const number = 121
+// const anotherNumber = 12.1
+
+// const name1 = "Sam"
+
+// const isTrue = true;
+
+// const outsideTemp = null; (outsideTemp = "")
+
+// let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+// const bigNumber = 1234556789734743n;
+
+
 
 // 2.Reference(Non-Primitive):
 // 3types:- Array,Object and Function
@@ -14,3 +33,31 @@ are:- Number, Boolean, string, null, undefined, symbol, bigInt.
 
 
 
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+              // ++++++++++++MEMORY++++++++++++
+   // Stack(Primitive) , Heap(Non-Primitive)  
+
+            // Stack (Primitive)
+let myYoutubename = "vlogwithSam"
+let anotherName = myYoutubename;
+anotherName = "samvlog"
+console.log(myYoutubename);
+console.log(anotherName);
+
+
+
+// Heap (Non-Primitive)
+
+let userOne = {
+    Email: "user@gmail.com",
+    upi: "abc@apl"
+}
+let userTwo = userOne;
+userTwo.Email = "sam@gmail.com"
+userTwo.upi = "sam@apl"
+console.log(userTwo);
