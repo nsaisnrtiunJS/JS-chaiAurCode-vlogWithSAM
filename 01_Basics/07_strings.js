2414c1c95725f374dc681ff38114d72477d9655b
SamCodrr <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-12-05 13:28:26 +0000

    strings

---
+++ 01_Basics/07_strings.js
@@ -0,0 +1,6 @@
+const name = "Sam"
+const lastName = "Islam"
+const repoCount = 50;
+
+// console.log("My name is " + name + " " + lastName + " and my repo count is " + repoCount);
+console.log(`My name is ${name.ToUpperCase} ${lastName} and My repo count is ${repoCount}`);
