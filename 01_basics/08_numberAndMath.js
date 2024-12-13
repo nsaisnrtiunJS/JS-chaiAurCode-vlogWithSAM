141b9bb04a2c38a5467237786b88ce8b5bd8f688
SamCodrr <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-12-13 18:26:21 +0000

    numberAndMath

---
+++ 01_basics/08_numberAndMath.js
@@ -0,0 +1,43 @@
+/////++++++++++++++ NUMBERS ++++++++++++++++/////
+
+// const number = 400
+// console.log(number)
+
+/*     const newNumber = new Number(400)
+ console.log(newNumber.toString())
+ console.log(newNumber.toString().length)    */
+
+/* using toFixed() function the result will appear decimel if you write 2 then result will 100.00 like this      
+console.log(newNumber.toFixed(2))
+console.log(newNumber.toFixed(4))    */
+
+/* const balance = 902.874902
+console.log(balance.toPrecision(5))
+console.log(balance.toPrecision(4))
+console.log(balance.toPrecision(2)) */
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+////////+++++++++++++++++++ MATH ++++++++++++++++++++++////////
+
+// console.log(Math)
+// console.log(Math.PI)
+// console.log(Math.abs(-5))
+// console.log(Math.abs(5))
+// console.log(Math.round(2.51))
+// console.log(Math.floor(2.98))
+// console.log(Math.ceil(2.11))
+// console.log(Math.min(2,9,4,1))
+// console.log(Math.max(2,9,4,1))
+
+console.log(Math.random())
+console.log((Math.random() *10) +1)
+console.log(Math.floor(Math.random() *10) +11)
+
+// Alternate 
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min) +min))
